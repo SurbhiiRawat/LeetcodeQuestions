30db06115b8e833432c367a85e45d0903798a566
Surbhi Rawat <146544089+SurbhiiRawat@users.noreply.github.com>
2025-02-27 21:47:01 +0530

    Time: 2 ms (97.61%), Space: 42 MB (56.12%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.java
@@ -4,11 +4,12 @@ class Solution {
         int i=1;
         int j=1;
         while(i < arr.length && j<arr.length-1){
-            if(arr[i]-arr[i-1] != arr[j+1]-arr[j]){
+            if(arr[i]-arr[i-1] == arr[j+1]-arr[j]){
+                i++;
+                j++;
+            }else{
                 return false;
             }
-            i++;
-            j++;
         }
         return true;
         
