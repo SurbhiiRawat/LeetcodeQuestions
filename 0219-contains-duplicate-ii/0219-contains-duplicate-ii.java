cf089f23b3356b8da8dc365e11f7ca4d432ef6e7
Surbhi Rawat <146544089+SurbhiiRawat@users.noreply.github.com>
2025-03-05 11:49:30 +0530

    Time: 19 ms (54.6%), Space: 55.3 MB (86.68%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,17 +1,31 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                if(Math.abs(map.get(nums[i]) - i) <= k){
-                    return true;
-                }
-            }
-            map.put(nums[i], i);
-            
-            //map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     if(map.containsKey(nums[i])){
+        //         if(Math.abs(map.get(nums[i]) - i) <= k){
+        //             return true;
+        //         }
+        //     }
+        //     map.put(nums[i], i);
+        // }
+        // return false;
 
+
+// use hash set :
+
+        Set <Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+            if(set.size() > k){
+                set.remove(nums[i-k]);
+            }
+            
         }
         return false;
+
     }
 }
