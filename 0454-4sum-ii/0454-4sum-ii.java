6c1af0a6d5eea37b2bbf7dd4ca1c240887743302
Surbhi Rawat <146544089+SurbhiiRawat@users.noreply.github.com>
2025-03-06 10:35:07 +0530

    Time: 107 ms (94.08%), Space: 44.9 MB (63.23%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2= new HashMap<>();
+       
         int ans =0;
 
         for(int i=0; i<nums1.length; i++){
@@ -11,15 +11,17 @@ class Solution {
         }
         for(int i=0; i<nums3.length; i++){
             for(int j = 0; j<nums4.length; j++){
-                map2.put(nums3[i]+nums4[j], map2.getOrDefault(nums3[i] + nums4[j], 0) +1);
+                int sum = nums3[i]+ nums4[j];
+                
+                ans += map1.getOrDefault(-sum, 0);
             }
         }
-        for(int value : map1.keySet()){
-            int val2 = -value;
-            if(map2.containsKey(val2)){
-                ans += map1.get(value) * map2.get(val2);
-            }
-        }
+        // for(int value : map1.keySet()){
+        //     int val2 = -value;
+        //     if(map2.containsKey(val2)){
+        //         ans += map1.get(value) * map2.get(val2);
+        //     }
+        // }
         return ans;
     }
 }
