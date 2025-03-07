8008a15cd5c256490e3b24f391351412a448465a
Surbhi Rawat <146544089+SurbhiiRawat@users.noreply.github.com>
2025-03-07 16:52:38 +0530

    Time: 2 ms (97.95%), Space: 42.4 MB (9.21%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -4,11 +4,15 @@ class Solution {
         for(int i=0; i<arr.length; i++){
             map.put(arr[i], map.getOrDefault(arr[i],0)+1);
         }
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<arr.length; i++){
-            set.add(map.get(arr[i]));
-        }
-        if(map.size() == set.size()) return true;
-        return false;
+        // Set<Integer> set = new HashSet<>();
+        // for(int i=0; i<arr.length; i++){
+        //     set.add(map.get(arr[i]));
+        // }
+        // if(map.size() == set.size()) return true;
+        // return false;
+
+        Set<Integer> set = new HashSet<>(map.values());
+        return map.size()==set.size();
+        
     }
 }
