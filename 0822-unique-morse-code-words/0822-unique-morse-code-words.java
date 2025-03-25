5a22ee7c1ceb3efc762ed968db98d30bb36a353f
Surbhi Rawat <146544089+SurbhiiRawat@users.noreply.github.com>
2025-03-25 22:03:02 +0530

    Time: 3 ms (37.99%), Space: 41.6 MB (81.51%) - LeetHub

---
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashMap<Character, String> mp = new HashMap<>();
+        mp.put('a', ".-");
+        mp.put('b', "-...");
+        mp.put('c', "-.-.");
+        mp.put('d', "-..");
+        mp.put('e', ".");
+        mp.put('f', "..-.");
+        mp.put('g', "--.");
+        mp.put('h', "....");
+        mp.put('i', "..");
+        mp.put('j', ".---");
+        mp.put('k', "-.-");
+        mp.put('l', ".-..");
+        mp.put('m', "--");
+        mp.put('n', "-.");
+        mp.put('o', "---");
+        mp.put('p', ".--.");
+        mp.put('q', "--.-");
+        mp.put('r', ".-.");
+        mp.put('s', "...");
+        mp.put('t', "-");
+        mp.put('u', "..-");
+        mp.put('v', "...-");
+        mp.put('w', ".--");
+        mp.put('x', "-..-");
+        mp.put('y', "-.--");
+        mp.put('z', "--..");
+
+        Set<String> set = new HashSet<>();
+        for(int i=0; i<words.length; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<words[i].length(); j++){
+                sb.append(mp.get(words[i].charAt(j)));  
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
